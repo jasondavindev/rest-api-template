b2630e7b4682252cf0e7b1f1edf7c02fe703329f
jasondavindev <jasondavin.dev@gmail.com>
2020-03-05 16:52:42 -0300

    fix eslint command

--- src/decorators/Logger.ts
+++ src/decorators/Logger.ts
@@ -1,6 +1,7 @@
 import { Logger as WinstonLogger } from '@/lib/logger'
 
 export function Logger(scope) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (object: Record<string, any>, propertyName: string) => {
     // eslint-disable-next-line no-param-reassign
     object[propertyName] = new WinstonLogger(scope)
--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as path from 'path'
 import * as winston from 'winston'
 
